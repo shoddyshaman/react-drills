53e2fab7c9d41eaf17a1409c5586e6318fabc5a0
shoddyshaman <djtiwari90@gmail.com>
2020-12-26 14:30:54 -0700

    uploading solved lab react-drills

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  constructor() {
+    super();
+
+    this.state = {
+      filterString:'',
+      ninjas: [ 'naruto','sasuke','shikamaru','hinata','choji']
+    }
+  }
+
+  handleChange(filter) {
+    this.setState({ filterString: filter });
+  }
+  
+  render () {
+    let ninjasToDisplay = this.state.ninjas
+    .filter((element,index) =>{
+      return element.includes(this.state.filterString)
+    })
+    .map((element,index) => {
+      return <h2 key={index}>{element}</h2>
+    })
+
+    return (
+      <div className="App"><br/>
+        <input onChange={ e => this.handleChange(e.target.value)} type = 'text' />
+        {ninjasToDisplay}
+      </div>
+    )
+  }
 }
 
 export default App;
--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Todo from "./Todo";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      list: [],
+      input: ""
+    };
+
+    this.handleAddTask = this.handleAddTask.bind(this);
+  }
+
+  handleInputChange(value) {
+    this.setState({ input: value });
+  }
+
+  handleAddTask() {
+    this.setState({
+      list: [...this.state.list, this.state.input],
+      input: ""
+    });
+  }
+
+  render() {
+    let list = this.state.list.map((element, index) => {
+      return <Todo key={index} task={element} />;
+    });
+
+    return (
+      <div className="App">
+        <h1>My to-do list:</h1>
+
+        <div>
+          <input
+            value={this.state.input}
+            onChange={e => this.handleInputChange(e.target.value)}
+          />
+
+          <button onClick={this.handleAddTask}>Add</button>
+        </div>
+
+        <br />
+
+        {list}
+      </div>
+    );
+  }
 }
 
 export default App;
